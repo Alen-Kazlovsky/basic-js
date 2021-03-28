b3d8170da80d19b0c7584b2a3f45fb18298251b3
Alen Kazlovsky <49515281+Alen-Kazlovsky@users.noreply.github.com>
2021-03-28 20:30:49 +0300

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  if  (isNaN(date.getTime())) {
+    throw new Error('Error')
+  };
+
+  let month = date.getMonth();
+  let day = date.getDay();
+
+  if  (1 < month &&  month < 5) {
+    return 'spring'
+  };
+  if  (4 < month &&  month < 8) {
+    return 'summer'
+  };
+  if  (7 < month &&  month < 11) {
+    return 'autumn'
+  };
+  if  (10 < month ||  month < 2) {
+    return 'winter'
+  };
 };
