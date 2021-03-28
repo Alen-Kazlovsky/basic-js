cb8f91c2d1ffb55728eb4441e78347b53f914486
Alen Kazlovsky <49515281+Alen-Kazlovsky@users.noreply.github.com>
2021-03-28 20:34:02 +0300

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    this.type = type;
+  }
+
+  encrypt (string, key) {
+    if (!string || !key) throw new Error('Error')
+
+    const arrAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const arrString = string.toUpperCase().split('');
+    const arrKey = key.toUpperCase().split('');
+    let arrAnswer =[];
+    let count = 0;
+    
+    arrString.forEach(elem => {
+      if (arrAlphabet.indexOf(elem) != -1) {
+        let indexKey = arrKey[count % arrKey.length];
+        arrAnswer.push(arrAlphabet[((arrAlphabet.indexOf(elem) + arrAlphabet.indexOf(indexKey)) % 26)]);
+        count++;
+      } else {
+        arrAnswer.push(elem);
+      }
+    })
+    return (this.type === true) ? arrAnswer.join('') : arrAnswer.reverse().join('');
+  }
+
+  decrypt (string, key) {
+    if (!string || !key) throw new Error('Error')
+
+    const arrAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const arrString = string.toUpperCase().split('');
+    const arrKey = key.toUpperCase().split('');
+    let arrAnswer =[];
+    let count = 0;
+
+    arrString.forEach(elem => {
+      if (arrAlphabet.indexOf(elem) != -1) {
+        let indexKey = arrKey[count % arrKey.length];
+        arrAnswer.push(arrAlphabet[((arrAlphabet.indexOf(elem) - arrAlphabet.indexOf(indexKey) + 26) % 26)]);
+        count++;
+      } else {
+        arrAnswer.push(elem);
+      }
+    })
+    return (this.type === true) ? arrAnswer.join('') : arrAnswer.reverse().join('');
   }
 }
-
 module.exports = VigenereCipheringMachine;
